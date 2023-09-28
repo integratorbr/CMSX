f519872fc8f73efe13b62df916ec8cdde752909d
marcio.campos <marcio.campos@systax.com.br>
2023-09-27 23:31:03 -0300

    estruturando diagramas

--- CMSX/CMSXEF/cmsxDB.Designer.cs
+++ CMSX/CMSXEF/cmsxDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Developer\CMSX\CMSX\CMSXEF\cmsxDB.edmx'. 
+﻿// T4 code generation is enabled for model 'T:\Developer\RepositorioEstudos\CMSX\CMSX\CMSXEF\cmsxDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
